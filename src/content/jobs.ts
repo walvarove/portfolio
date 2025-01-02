3029ce057f79f098bb5651947601366aff57493f
walvarove <pedroponcealvaro@gmail.com>
2025-01-02 14:34:40 +0100

    First ocmmit

---
+++ src/content/jobs.ts
@@ -0,0 +1,33 @@
+import { differenceInMonths, differenceInYears } from "date-fns";
+
+type Job = {
+  company: string;
+  link: string;
+  start: string;
+  end: string;
+  description: string;
+  job: string;
+  teckStack: string[];
+};
+
+const calculateDuration = (start: string, end: string | null) => {
+  const startDate = new Date(start);
+  const endDate = end ? new Date(end) : new Date();
+  const diffMonths = differenceInMonths(endDate, startDate);
+  const diffYears = differenceInYears(endDate, startDate);
+
+  if (diffYears > 0) {
+    return diffYears + " years and " + (diffMonths % 11) + " months";
+  } else {
+    return diffMonths + " months";
+  }
+};
+
+const toSimpleDate = (startDate: string) => {
+  return new Date(startDate).toLocaleDateString("en-US", {
+    month: "short",
+    year: "numeric",
+  });
+};
+
+export { calculateDuration, toSimpleDate };
---
+++ src/content/sections/summary.md
@@ -0,0 +1,8 @@
+---
+key: "summary"
+---
+Personally, I think that I am a cheerful, friendly, humble and polite person. I value above all the easy way with all the people around me or to whom I address. On the other hand, I am also responsible, ambitious and I have great eagerness to achieve what I propose, I rarely leave problems (however difficult they may be) unsolved.
+
+Among my hobbies I highlight: My love for music and sports (particularly power sports), and my passion for technology and computers.
+
+I am currently finishing my 4th year of Computer Science, and I would like to direct my studies and training towards aspects such as data science, machinery learning,robotics...
